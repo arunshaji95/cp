fd541279ed86f2c3ac31e3a2dacd0e30ecc85dd2
Arun Shaji <arunshaji4230@gmail.com>
2023-10-24 19:25:26 +0530

    added stack implementation
    Signed-off-by: Arun Shaji <arunshaji4230@gmail.com>

--- leaders-in-an-array-23102023/scan.py
+++ leaders-in-an-array-23102023/scan.py
@@ -1,6 +1,10 @@
 input_array = [16, 17, 4, 3, 5, 2]
 maximum = 0
+stack = []
 for i in range(len(input_array)-1, -1, -1):
     if input_array[i] > maximum:
-        print(input_array[i])
+        stack.append(input_array[i])
     maximum += input_array[i]
+while stack:
+    print(stack.pop(), end=",")
+print()
