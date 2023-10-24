5b35730725d0df0b9363cbfa3a06775755180618
Arun Shaji <arunshaji4230@gmail.com>
2023-10-24 19:27:40 +0530

    updated repo structure
    Signed-off-by: Arun Shaji <arunshaji4230@gmail.com>

--- 23102023-leaders-in-an-array/scan.py
+++ 23102023-leaders-in-an-array/scan.py
@@ -1,3 +1,8 @@
+# Write a program to print all the LEADERS in the array. 
+# An element is a leader if it is greater than all the elements to its right side. 
+# And the rightmost element is always a leader. 
+# Time complexity is O(n)
+
 input_array = [16, 17, 4, 3, 5, 2]
 maximum = 0
 stack = []
